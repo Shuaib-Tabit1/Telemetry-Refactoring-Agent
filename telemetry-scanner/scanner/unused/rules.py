b6844a13608aaa294e811dcf056d3aae86fd47fc
Shuaib Tabit <shuaib.tabit@docusign.com>
2025-08-19 11:02:54 -0700

    Initial commit

--- telemetry-scanner/scanner/unused/rules.py
+++
@@ -1,7 +0,0 @@
-# TODO: Implement telemetry rule loading logic from telemetry_rules.yaml.
-# This module will parse the YAML file into a structured set of rules
-# that can be consumed by the static scanner (finder.py).
-
-def load_rules(path: str):
-    """Loads and validates rules from a YAML file."""
-    pass
